8e8097b97c576e2ffa21a89187c8aee74784ce9b
mr.valerio <valera.osadchuk@gmail.com>
2024-06-06 19:21:10 +0300

    trying to implement test for time overlap (unsuccessfully yet)

--- components/Form/User/Event.vue
+++ components/Form/User/Event.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { EventKinds, type Event } from "@/types";
+import { timeToPlainNumber, isTimeOverlapIntervals } from "~/helpers";
 
 import { useEmployeesStore } from "~/stores/employees";
 const employeeStore = useEmployeesStore();
@@ -23,6 +24,21 @@ const handleDeleteEvent = () => {
   emit("deleteEvent", event.value.id);
   employeeStore.deleteEvent(state.employeeId, event.value.id);
 };
+
+//================================================
+// TODO: need to think about how to implement this properly
+// const events = employeeStore.getEmployeeEventsByDate(
+//   state.employeeId!,
+//   event.value.date
+// );
+// const isOverlap = computed(
+//   () =>
+//     isTimeOverlapIntervals(
+//       timeToPlainNumber(event.value.period.start),
+//       events,
+//       event.value.kind
+//     )
+// );
 </script>
 
 <template>
@@ -37,18 +53,11 @@ const handleDeleteEvent = () => {
       class="space-y-4 bg-indigo-100 dark:bg-indigo-900 text-indigo-900 dark:text-indigo-200 py-3 px-2 rounded"
     >
       <div class="grid grid-cols-3 gap-x-2">
-        <UFormGroup
-          label="start"
+        <UIInputTime
+          v-model="event.period.start"
+          label="Start time"
           :name="`events[${index}].period.start`"
-          :ui="{ label: { base: 'text-indigo-900' } }"
-        >
-          <UInput
-            v-model="event.period.start"
-            type="Start time"
-            icon="i-heroicons-clock"
-            trailing
-          />
-        </UFormGroup>
+        />
         <UIInputTime
           v-model="event.period.end"
           label="End time"
@@ -63,6 +72,13 @@ const handleDeleteEvent = () => {
           />
         </UFormGroup>
       </div>
+
+      <UAlert
+        v-if="false"
+        variant="outline"
+        color="amber"
+        title="You already have appointment at this time period"
+      />
 
       <UFormGroup
         label="Kind"
--- helpers/index.ts
+++ helpers/index.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from "uuid";
 import { format } from "date-fns";
-import type { Event, Employee, Events } from "../types";
+import type { Event, Employee, Events, EventKinds } from "../types";
 
 import { type EmployeeSchema } from "~/schemas";
 
@@ -34,3 +34,35 @@ export const sortEventsByDate = (
     const dateB = new Date(b.date).getTime();
     return direction === "asc" ? dateA - dateB : dateB - dateA;
   });
+
+export const timeToPlainNumber = (time: string): number =>
+  parseInt(time.replace(":", ""));
+
+export interface PlainTimePeriod {
+  start: number;
+  end: number;
+}
+
+export const getEventsTimeIntervals = (
+  events: Events,
+  kind: EventKinds | undefined
+): PlainTimePeriod[] | null => {
+  if (!kind) return null;
+  return events
+    .filter((events) => events.kind === kind)
+    .map((event) => ({
+      start: timeToPlainNumber(event.period.start),
+      end: timeToPlainNumber(event.period.end),
+    }));
+};
+
+export const isTimeOverlapIntervals = (
+  time: number,
+  events: Events,
+  kind: EventKinds | undefined
+): boolean => {
+  const intervals = getEventsTimeIntervals(events, kind);
+  if (!intervals) return false;
+
+  return intervals.some((e) => time >= e.start && time <= e.end);
+};
